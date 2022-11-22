c022c4658b6a2e0488b9871d0b341aaaf8922a96
jordan houyon <116800565+Jordan-Houyon@users.noreply.github.com>
2022-11-22 16:41:26 +0100

    exo 77 ok

--- exo 2/exercice2.js
+++ exo 2/exercice2.js
@@ -1,4 +1,8 @@
 /* Créer ci dessous une fonction appelé myFunction qui éxécutera le code suivant puis appeler cette fonction */
+function myFunction(){
+    alert("Je viens d'appeler ma fonction");
+    return(myFunction());
+}
 
 
 alert("Je viens d'appeler ma fonction"); // Code a exécuter dans la fonction.
--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,10 +1,13 @@
 /* Ecrire ci dessous une fonction permettant d'ajouter du texte dans le div appelé container */
 
+function addText(text) {
+    let container = document.getElementById("container");
+    container.innerText = text;
+}
 
 
 /* Appeler la fonction */
 
 
-
-
-
+x = prompt()
+addText(x)
